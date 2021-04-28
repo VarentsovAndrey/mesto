43db0a402bd4f10ab264a5aff8270c2d358ebbde
Varentsov Andrey <varentsov.andrey@gmail.com>
2021-04-28 08:51:36 +0300

    class Card

--- index.js
+++ index.js
@@ -104,46 +104,47 @@ popupPreviewClose.addEventListener("click", () => {
   closeModal(popupPreview);
 });
 
-function render() {
-  initialCards.forEach(function (element) {
-    const card = createCard(element);
-    cardAddElement(card);
-  });
-}
+// function render() {
+//   initialCards.forEach(function (element) {
+//     const card = new Card(element);
+//     cardAddElement(card);
+//   });
+// }
 
-function createCard(element) {
-  const htmlElement = itemTemplate.cloneNode(true);
-  htmlElement.querySelector(".elements__title").textContent = element.name;
+// function createCard(element) {
+// const htmlElement = itemTemplate.cloneNode(true);
+// htmlElement.querySelector(".elements__title").textContent = element.name;
 
-  const elementItem = htmlElement.querySelector(".elements__item");
+// const elementItem = htmlElement.querySelector(".elements__item");
 
-  elementItem.src = element.link;
-  elementItem.alt = "Новая карточка";
+// elementItem.src = element.link;
+// elementItem.alt = "Новая карточка";
 
-  htmlElement
-    .querySelector(".elements__delete")
-    .addEventListener("click", handleDelete);
-  htmlElement
-    .querySelector(".elements__button")
-    .addEventListener("click", function (evt) {
-      evt.target.classList.toggle("elements__button_active", true);
-    });
+// htmlElement
+//   .querySelector(".elements__delete")
+//   .addEventListener("click", handleDelete);
 
-  htmlElement
-    .querySelector(".elements__item")
-    .addEventListener("click", handlePopup);
+// htmlElement
+//   .querySelector(".elements__button")
+//   .addEventListener("click", function (evt) {
+//     evt.target.classList.toggle("elements__button_active", true);
+//   });
 
-  function handlePopup(evt) {
-    openModal(popupPreview);
-    popupPreviewTitle.textContent = element.name;
-    popupPreviewImg.src = element.link;
-  }
-  function handleDelete(evt) {
-    evt.target.closest(".elements__element").remove();
-  }
+// htmlElement
+//   .querySelector(".elements__item")
+//   .addEventListener("click", handlePopup);
 
-  return htmlElement;
-}
+// function handlePopup(evt) {
+//   openModal(popupPreview);
+//   popupPreviewTitle.textContent = element.name;
+//   popupPreviewImg.src = element.link;
+// }
+// function handleDelete(evt) {
+//   evt.target.closest(".elements__element").remove();
+// }
+
+//   return htmlElement;
+// }
 
 const cardAddElement = function (element) {
   elements.prepend(element);
@@ -162,7 +163,7 @@ function handleAddCard(evt) {
 
 addCards.addEventListener("click", handleAddCard);
 
-render();
+// render();
 
 const popups = document.querySelectorAll(".popup");
 
@@ -180,3 +181,172 @@ function closePopupEsc(evt) {
     closeModal(popupIsOpen);
   }
 }
+
+class Card {
+  constructor(element) {
+    this._element = element;
+    this._htmlElement = this._getTemplate();
+  }
+  _makeEventListener() {
+    this._htmlElement
+      .querySelector(".elements__delete")
+      .addEventListener("click", () => this._removeCard());
+    this._htmlElement
+      .querySelector(".elements__button")
+      .addEventListener("click", () => this._likeCard());
+    this._htmlElement
+      .querySelector(".elements__item")
+      .addEventListener("click", () => this._previewCard());
+  }
+
+  _getTemplate() {
+    const htmlElement = itemTemplate.cloneNode(true);
+    const elementItem = htmlElement.querySelector(".elements__item");
+    htmlElement.querySelector(
+      ".elements__title"
+    ).textContent = this._element.name;
+    elementItem.src = this._element.link;
+    elementItem.alt = "Новая карточка";
+
+    return htmlElement;
+  }
+
+  _likeCard() {
+    _htmlElement
+      .querySelector(".elements__button")
+      .target.classList.toggle("elements__button_active", true);
+  }
+
+  _removeCard() {
+    htmlElement.remove();
+  }
+
+  _previewCard() {
+    openModal(popupPreview);
+    popupPreviewTitle.textContent = this._element.name;
+    popupPreviewImg.src = this._element.link;
+  }
+  getElement() {
+    this._makeEventListener();
+    return this._htmlElement;
+  }
+}
+
+initialCards.forEach((element) => {
+  renderCard(element);
+});
+
+function renderCard(element) {
+  const card = new Card(element)._getTemplate();
+  cardAddElement(card);
+}
+
+class Validate {
+  constructor() {}
+
+  _showInputError(formPopup, formInput, errorMessage) {
+    const formError = this._formPopup.querySelector(`#${formInput.id}-error`);
+    formInput.classList.add("popup__input_type_error");
+    formError.textContent = errorMessage;
+    formError.classList.add("popup__error_active");
+  }
+
+  _hideInputError(formPopup, formInput) {
+    const formError = this._formPopup.querySelector(`#${formInput.id}-error`);
+    formInput.classList.remove("popup__input_type_error");
+    formError.textContent = "";
+    formError.classList.remove("popup_error_active");
+  }
+
+  _toggleButtonState(inputList, buttonPopup) {
+    if (this._hasInvalidInput(inputList)) {
+      buttonPopup.classList.add("popup__button_disabled");
+      buttonPopup.setAttribute("disabled", true);
+    } else {
+      buttonPopup.classList.remove("popup__button_disabled");
+      buttonPopup.removeAttribute("disabled");
+    }
+  }
+
+  _checkInput(formPopup, formInput) {
+    if (!formInput.validity.valid) {
+      this._showInputError(formPopup, formInput, formInput.validationMessage);
+    } else {
+      this._hideInputError(formPopup, formInput);
+    }
+  }
+
+  _setEventListeners(formPopup) {
+    const inputList = Array.from(
+      this._formPopup.querySelectorAll(".popup__input")
+    );
+    const buttonPopup = this._formPopup.querySelector(".popup__button");
+    inputList.forEach((formInput) => {
+      formInput.addEventListener("input", () => {
+        checkInput(formPopup, formInput);
+        toggleButtonState(inputList, buttonPopup);
+      });
+    });
+  }
+}
+
+// const hasInvalidInput = (inputList) => {
+//   return inputList.some((formInput) => !formInput.validity.valid);
+// };
+
+// const toggleButtonState = (inputList, buttonPopup) => {
+//   if (hasInvalidInput(inputList)) {
+//     buttonPopup.classList.add("popup__button_disabled");
+//     buttonPopup.setAttribute("disabled", true);
+//   } else {
+//     buttonPopup.classList.remove("popup__button_disabled");
+//     buttonPopup.removeAttribute("disabled");
+//   }
+// };
+
+// const showInputError = (formPopup, formInput, errorMessage) => {
+//   const formError = formPopup.querySelector(`#${formInput.id}-error`);
+//   formInput.classList.add("popup__input_type_error");
+//   formError.textContent = errorMessage;
+//   formError.classList.add("popup__error_active");
+// };
+
+// const hideInputError = (formPopup, formInput) => {
+//   const formError = formPopup.querySelector(`#${formInput.id}-error`);
+//   formInput.classList.remove("popup__input_type_error");
+//   formError.textContent = "";
+//   formError.classList.remove("popup_error_active");
+// };
+
+// const checkInput = (formPopup, formInput) => {
+//   if (!formInput.validity.valid) {
+//     showInputError(formPopup, formInput, formInput.validationMessage);
+//   } else {
+//     hideInputError(formPopup, formInput);
+//   }
+// };
+
+// const setEventListeners = (formPopup) => {
+//   const inputList = Array.from(formPopup.querySelectorAll(".popup__input"));
+//   const buttonPopup = formPopup.querySelector(".popup__button");
+//   inputList.forEach((formInput) => {
+//     formInput.addEventListener("input", () => {
+//       checkInput(formPopup, formInput);
+//       toggleButtonState(inputList, buttonPopup);
+//     });
+//   });
+// };
+
+// const enableValidation = () => {
+//   const formList = Array.from(document.querySelectorAll(".popup__form"));
+
+//   formList.forEach((formPopup) => {
+//     formPopup.addEventListener("submit", (evt) => {
+//       evt.preventDefault();
+//     });
+
+//     setEventListeners(formPopup);
+//   });
+// };
+
+// enableValidation();
